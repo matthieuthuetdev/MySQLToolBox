c85848bb6302ec5895e2f3e153238f69aa469547
MatthieuThuet <mthuet.pro@gmail.com>
2025-03-30 12:15:38 +0200

    f

--- src/MyToolBox.php
+++ src/MyToolBox.php
@@ -1,18 +1,24 @@
 <?php
+
+namespace Matthieuthuetdev\MyToolBox;
+
 use PDO;
+
 class MyToolBox
 {
-    private  $connection;
+    private $connection;
+
     public function __construct(PDO $pdoInstance)
     {
         $this->connection = $pdoInstance;
     }
-    public function exportDatabase(){
-        $requrest = "SHOW TABLES";
-        $rq = $this->connection->prepare($requrest);
+
+    public function exportDatabase()
+    {
+        $request = "SHOW TABLES";
+        $rq = $this->connection->prepare($request);
         $rq->execute();
         $result = $rq->fetchAll(PDO::FETCH_ASSOC);
         return $result;
-
     }
 }
